55ecb82b2daca820f51c3df0b4caa56695a0981c
スタークギャング <StarkGangZ@gmail.com>
2021-05-30 06:05:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
     PORT = int(os.environ.get("PORT", "8443"))
     HOST = os.environ.get("HOST", "0.0.0.0")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    PING_TIME = int(os.environ.get("PING_TIME", 10))
